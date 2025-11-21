01597c0314f29be78d3a3eb7f4fece6a540315c6
mhmd2900 <mohamedsad2900@gmail.com>
2025-11-21 20:11:28 +0300

    Update lessons.cpp

--- lessons.cpp
+++ lessons.cpp
@@ -90,25 +90,24 @@ cout << " \n \n Time: " << ms << " µs\n";
 
 ////////////////////    printf
 int a = 5 ;
-float d = 2.0 ;
 int u = 50 ;
-double f = 4.123456 ;
-printf(" the a = %d \n " ,  a ) ;   // 5
-printf(" also a equals = %0*d \n \n  " , 2  , a ) ; // 05
-printf(" also a equals = %0*d \n \n  " , 2  , u ) ; // 50
-printf(" also a equals = %02d \n \n  " , u ) ; // 50              *********
+printf("   the a = %d \n  " ,  a ) ;   // 5
+printf(" also a equals = %0*d \n  " , 2  , a ) ; // 05
+printf(" also a equals = %0*d \n  " , 2  , u ) ; // 50
+printf(" also a equals = %02d \n  " , u ) ; // 50              *********
 
+float d = 2.0 ;
+double f = 4.123456 ;
 printf ( " the d = %.4f \n" ,   d );  // 2.0000                  **********
 printf ( " the d = %.4f \n" ,   f );   // 4.1235
 printf ( " and the f equals = %.*f \n" , 4 , d );  // 2.0000
-printf ( " and the f equals = %.*f \n \n" , 4 , f );  // // 4.1235
-
+printf ( " and the f equals = %.*f \n " , 4 , f );  // // 4.1235
 
 char name [] = " mhmd";
 char letter = 'm';
-printf(" my name is %s \n " , name );  // my name is mhmd
-printf(" my letter is %*c \n " , 2 , letter) ;  // my letter is  m
-printf(" my letter is %*c \n " , 6 , letter) ;  // my letter is       m
+printf("  my name is %s \n " , name );  // my name is mhmd
+printf(" my letter is %*c \n " , 6 , letter) ;  // my letter is      m
+printf(" my letter is %6c \n " , letter) ;  //     my letter is      m
 
 
 
